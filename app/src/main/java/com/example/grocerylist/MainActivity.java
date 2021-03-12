8aa8cc875bf03dc310ec70a63f024eed37994f0a
wafflethief <janlouie810@gmail.com>
2021-03-12 10:03:37 -0500

    add button added

--- app/src/main/java/com/example/grocerylist/MainActivity.java
+++ app/src/main/java/com/example/grocerylist/MainActivity.java
@@ -5,12 +5,15 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     ArrayList<Item> list;
+    Button addItemButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +26,17 @@ public class MainActivity extends AppCompatActivity {
         ItemsAdapter adapter = new ItemsAdapter(list);
         rvItems.setAdapter(adapter);
         rvItems.setLayoutManager(new LinearLayoutManager(this));
+        addItemButton = (Button) findViewById(R.id.add_button);
+        addItemButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int curSize = adapter.getItemCount();
+
+                ArrayList<Item> newList = Item.createList(1);
+
+                list.addAll(newList);
+                adapter.notifyItemRangeInserted(curSize, newList.size());
+            }
+        });
     }
 }
